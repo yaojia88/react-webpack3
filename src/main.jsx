0be0e1788531ae87ad7220a220ebafc4db311538
yaojia <yaojia1220@126.com>
2017-08-21 21:06:19 +0800

    add select time

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,16 @@ import moment from 'moment';
 
 moment.locale('en');
 const mountNode = document.getElementById('app');
+const {MonthPicker, RangePicker} = DatePicker;
+
+function onchange(date,dateString) {
+    console.log(date,dateString);
+}
 
 ReactDOM.render(
   <div>
     <DatePicker defaultValue={moment()} />
+      <MonthPicker onchange={onchange} palceholder="Select Month" />
+      <RangePicker onchange={onchange} palceholder="StartTime,EndTime " />
   </div>
   , mountNode);
